a0e717168171fca7b810fcebb5dfd1a28abe2ac7
V School <vschool@Vs-Mac-Pro.local>
2017-08-14 15:06:45 -0600

    small changes

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class App extends React.Component {
                     <Navbar/>
                     <Switch>
                         <Route exact path="/profile" component={ProfileListContainer}/>
-                        <Route exact path="/home" component={BaseContainer} />
+                        <Route exact path="/" component={BaseContainer} />
                     </Switch>
                     <Footer/>
                     </div>
