158fd4db4712e75969b47c9b6aa7516e33fb3adb
Wyatt Blackhurst <dmonteblackhurst@gmail.com>
2017-08-17 11:59:05 -0600

    styling changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -41,7 +41,7 @@ class Navbar extends React.Component {
                         <li><Link className="link-title" to="/">Team Deathmatch</Link></li>
                         <li><Link className="link" to="/"><img style={imageStyle} className="profile-pic" src={this.props.currentUser.avatar}/>{profile}</Link></li>
                         <li><Link className="link" to="/search">Search</Link></li>
-                        <li><a style={signInStyle} className="link sign-in" href="auth/steam">Sign In</a></li>
+                        <li><a style={signInStyle} className="link sign-in" href="/auth/steam">Sign In</a></li>
                         <li><a style={signOutStyle} className="link sign-out" href="/">Sign Out</a></li>
                         <li><Link className="link" to="/developers">Developers</Link></li>
 
