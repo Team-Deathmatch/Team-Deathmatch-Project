5ef63347c9fe36446fb1c97dedcdf0a40f50933f
V School <vschool@Vs-Mac-Pro.local>
2017-08-18 09:52:45 -0600

    contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,14 +8,14 @@ class Contact extends React.Component {
                 <div className="row move-right">
                     <div className="col-lg-4">
                         <h3>Dakota Heninger</h3>
-                        <div className="dev"></div>
+                        <img src="/devs/Dakota" className="dev"/>
                         <div className="info">
                             <h4>Phone:
                             </h4>
-                            <p>845028</p>
+                            <p>385.288.7378</p>
                             <h4>Email:
                             </h4>
-                            <p>jlf;af;ha[fod</p>
+                            <p>dakota.heninger.22@gmail.com</p>
                             <h4>Github:</h4>
                             <span className="fa-stack fa-lg hub">
                                 <a href="https://github.com/dakotaheninger" target="_blank"><FontAwesome stack="2x" name="github"/></a>
@@ -31,9 +31,9 @@ I bet you’re still using Bootstrap too…</p>
                         </div>
                     </div>
 
-                    <div className="col-lg-4">
-                        <h3>Jordan Jensen</h3>
-                        <div className="dev"></div>
+                    <div className="col-lg-4 ">
+                        <h3 className="name">Jordan Jensen</h3>
+                        <img src="/devs/Jordan.jpg" className="dev"/>
                         <div className="info">
                             <h4>Phone:</h4>
                             <p>801-608-0266</p>
@@ -46,23 +46,20 @@ I bet you’re still using Bootstrap too…</p>
                             <h4 className="bio">Bio:</h4>
                         </div>
                         <div>
-                            <p>Lyft photo booth offal artisan, fingerstache pabst succulents next level cronut meggings palo santo squid venmo ugh semiotics. Shabby chic vegan tbh copper mug slow-carb. Tacos neutra cray, jianbing polaroid chia yuccie try-hard kickstarter intelligentsia synth. Franzen four loko gentrify selfies, trust fund cornhole scenester viral skateboard vinyl air plant kombucha ennui iPhone fanny pack. Edison bulb leggings direct trade, prism letterpress gochujang hell of mixtape XOXO hoodie squid copper mug raw denim post-ironic. Palo santo etsy gentrify waistcoat poke plaid roof party. Master cleanse air plant fixie XOXO drinking vinegar la croix. Health goth wayfarers edison bulb, pickled scenester wolf tbh helvetica readymade. Franzen shoreditch chicharrones brooklyn lyft pabst VHS fingerstache fam butcher messenger bag. Tote bag single-origin coffee art party direct trade, iceland cold-pressed fashion axe seitan vexillologist shoreditch taxidermy asymmetrical activated charcoal retro twee. Poutine iPhone austin four dollar toast pop-up.
-Oh. You need a little dummy text for your mockup? How quaint.
-
-I bet you’re still using Bootstrap too…</p>
+                            <p></p>
                         </div>
                     </div>
 
                     <div className="col-lg-4">
-                        <h3>Wyatt Blackhurst</h3>
-                        <div className="dev"></div>
+                        <h3 className="name">Wyatt Blackhurst</h3>
+                        <img src="/devs/Wyatt" className="dev"/>
                         <div className="info">
                             <h4>Phone:
                             </h4>
-                            <p>845028</p>
+                            <p>801.707.1905</p>
                             <h4>Email:
                             </h4>
-                            <p>jlf;af;ha[fod</p>
+                            <p>dmonteblackhurst@gmail.com</p>
                             <h4>Github:</h4>
                             <span className="fa-stack fa-lg hub">
                                 <a href="https://github.com/monteblack7" target="_blank"><FontAwesome stack="2x" name="github"/></a>
@@ -71,7 +68,7 @@ I bet you’re still using Bootstrap too…</p>
   <div>
                             <p>Lyft photo booth offal artisan, fingerstache pabst succulents next level cronut meggings palo santo squid venmo ugh semiotics. Shabby chic vegan tbh copper mug slow-carb. Tacos neutra cray, jianbing polaroid chia yuccie try-hard kickstarter intelligentsia synth. Franzen four loko gentrify selfies, trust fund cornhole scenester viral skateboard vinyl air plant kombucha ennui iPhone fanny pack. Edison bulb leggings direct trade, prism letterpress gochujang hell of mixtape XOXO hoodie squid copper mug raw denim post-ironic. Palo santo etsy gentrify waistcoat poke plaid roof party. Master cleanse air plant fixie XOXO drinking vinegar la croix. Health goth wayfarers edison bulb, pickled scenester wolf tbh helvetica readymade. Franzen shoreditch chicharrones brooklyn lyft pabst VHS fingerstache fam butcher messenger bag. Tote bag single-origin coffee art party direct trade, iceland cold-pressed fashion axe seitan vexillologist shoreditch taxidermy asymmetrical activated charcoal retro twee. Poutine iPhone austin four dollar toast pop-up.
 Oh. You need a little dummy text for your mockup? How quaint.
-
+ 
 I bet you’re still using Bootstrap too…</p>
                         </div>
                         </div>
